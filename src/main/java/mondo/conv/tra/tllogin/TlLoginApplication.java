a39b63ba8ce88e26cbe4844a98ebed504647f049
matteo13001 <matteo.lib.13@gmail.com>
2022-12-11 12:19:31 +0100

    prima versione

---
+++ src/main/java/mondo/conv/tra/tllogin/TlLoginApplication.java
@@ -0,0 +1,13 @@
+package mondo.conv.tra.tllogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mondo/conv/tra/tllogin/TlLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package mondo.conv.tra.tllogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
